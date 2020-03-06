787b5e859fe48005facd3c94c07a445a290394b1
isaiah <isaiah.mackey@digiPen.com>
2020-03-06 08:26:48 -0800

    nb
    Signed-off-by: isaiah <isaiah.mackey@digiPen.com>

---
+++ done/TextAdventureVisualStudio/ReadCommandHandler.h
@@ -0,0 +1,16 @@
+/*
+ReadCommandHandler.h
+By: Esther Strathy 
+3/4/2020
+Description: header file for read command handler
+*/
+
+
+
+#pragma once
+typedef struct CommandData CommandData;
+typedef struct GameState GameState;
+typedef struct WorldData WorldData;
+
+
+void HandleReadCommand();
