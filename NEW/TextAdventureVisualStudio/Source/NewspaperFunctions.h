d29c5064741fea3f3f3ea420c29a75bcee808098
isaiah <isaiah.mackey@digiPen.com>
2020-03-06 07:34:00 -0800

    X
    Signed-off-by: isaiah <isaiah.mackey@digiPen.com>

---
+++ NEW/TextAdventureVisualStudio/Source/NewspaperFunctions.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+filename    Newspaper.c
+author      Ethan Young
+DP email    ethan.young@digipen.edu but please use ethany905@gmail.com
+course      GAM100 ** Do not use this code in your team project
+
+Brief Description:
+This file declares the functions to create a specific item, the "newspaper".
+
+******************************************************************************/
+#pragma once
+
+typedef struct Item Item;
+
+
+/* Build a "Newspaper" object */
+Item* Newspaper_Build();
