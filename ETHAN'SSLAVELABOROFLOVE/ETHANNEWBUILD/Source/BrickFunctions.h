5aff32e69725db279e42e9a17a10903b3f499823
Younget <48969941+Younget@users.noreply.github.com>
2020-03-11 08:31:30 -0700

    pray to your own deity -EthanY

---
+++ ETHAN'SSLAVELABOROFLOVE/ETHANNEWBUILD/Source/BrickFunctions.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+filename    BrickFunctions.h
+author      Matthew Picioccio
+DP email    mattpic@digipen.edu
+course      GAM100 ** Do not use this code in your team project
+
+Brief Description:
+This file declares the functions to create a specific item, the "brick".
+All content © 2020 DigiPen (USA) Corporation, all rights reserved.
+******************************************************************************/
+#pragma once
+
+typedef struct Item Item;
+
+
+/* Build a "brick" object */
+Item* Brick_Build();
